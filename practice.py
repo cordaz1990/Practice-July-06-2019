75543286f22f0db3f89778fa19c6c7de5fbed0cf
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 18:09:14 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,10 @@
-print('hello')
-print('the')
-prin
+#Nested Conditionals
+
+if x == y:
+   print('x and y are equal')
+else:
+  if x < y:
+    print('x is less than y')
+  else:
+    print('x is greater than y')
+  
